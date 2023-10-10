a37a44345eb16ea4b0d03f850dc5b5f617b2f463
HPadg <h.padget@uq.net.au>
2023-10-10 21:36:49 +1000

    Implemented functionality for the learn page. Added content to the html file for the structure Added styles to css file for it to look nice Added functions to javascript file to handle creating the question and moving to new questions

---
+++ pages/script.js
@@ -0,0 +1,62 @@
+const questions = [
+    {
+        question: "How can you detect a phishing attempt from the text?",
+        options: ["Poor Grammer", "Incorrect Spelling", "Unprofessional Language", "All of the above"],
+        correctAnswer: "All of the above",
+    },
+    {
+        question: "What should you do when you receive a text with a link saying you have won the lottery?",
+        options: ["CLICK THE LINK!", "Ponder what you will do with the money", "Delete the text, it is probably a scam", "Celebrate! You just won the lottery!"],
+        correctAnswer: "Delete the text, it is probably a scam",
+    },
+];
+
+let currentQuestionIndex = 0;
+
+const questionElement = document.getElementById("question");
+const optionsContainer = document.getElementById("answers");
+const feedbackElement = document.getElementById("feedback");
+const nextButton = document.getElementById("nextButton");
+
+function loadQuestion() {
+    print(currentQuestionIndex)
+    const currentQuestion = questions[currentQuestionIndex];
+    questionElement.textContent = currentQuestion.question;
+
+    optionsContainer.innerHTML = "";
+    currentQuestion.options.forEach((option, index) => {
+        const optionButton = document.createElement("button");
+        optionButton.classList.add("option");
+        optionButton.textContent = option;
+        optionButton.addEventListener("click", () => checkAnswer(option));
+        optionsContainer.appendChild(optionButton);
+    });
+}
+
+function checkAnswer(selectedOption) {
+    const currentQuestion = questions[currentQuestionIndex];
+    if (selectedOption === currentQuestion.correctAnswer) {
+        feedbackElement.textContent = "Phishtacular!";
+    } else {
+        feedbackElement.textContent = "Wrong. The correct answer is: " + currentQuestion.correctAnswer;
+    }
+
+    nextButton.style.display = "block";
+}
+
+function nextQuestion() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        loadQuestion();
+        feedbackElement.textContent = "";
+        nextButton.style.display = "none";
+    } else {
+        questionElement.textContent = "Quiz Complete!";
+        optionsContainer.innerHTML = "";
+        feedbackElement.textContent = "";
+        nextButton.style.display = "none";
+    }
+}
+
+loadQuestion();
+nextButton.addEventListener("click", nextQuestion);
